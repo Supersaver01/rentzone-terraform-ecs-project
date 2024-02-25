5b86f533aba5574354fc24b3a42266ff595d48a8
Shefiu Yusuf <mailsupersaver@gmail.com>
2024-02-25 01:55:16 -0600

    created files, asg, ecs, ecs role, route53, s3, outputs

--- variables.tf
+++ variables.tf
@@ -104,3 +104,36 @@ variable "alternative_names" {
     type        = string
 
 }
+
+#s3 variables
+variable "env_file_bucket_name" {
+    description = "s3 bucket name"
+    type        = string
+
+}
+
+variable "env_file_name" {
+    description = "env file name"
+    type        = string
+
+}
+
+#ECS VARIABLES
+variable "cpu_architecture" {
+    description = "ecs-cpu-architecture"
+    type        = string
+
+}
+
+variable "container_image" {
+    description = "container_image_uri"
+    type        = string
+
+}
+
+#Route53 Variables
+variable "record_name" {
+    description = "subdomain name"
+    type        = string
+
+}
